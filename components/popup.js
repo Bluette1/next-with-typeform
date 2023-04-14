8b53a74fe33d4dce007e96eb8858a3b9e69ccb5a
Bluette1 <marylene.sawyer@gmail.com>
2023-04-14 07:58:02 +0200

    Add Next app with Typeform

---
+++ components/popup.js
@@ -0,0 +1,11 @@
+import { PopupButton } from "@typeform/embed-react";
+
+const Popup = () => {
+  return (
+    <PopupButton id="uCWQq4Vg" style={{ fontSize: 20 }} className="my-button">
+      click to open form in popup
+    </PopupButton>
+  );
+};
+
+export default Popup;
