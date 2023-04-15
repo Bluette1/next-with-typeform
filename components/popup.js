8411bda0dd9d53c6e273b7ee5e61d021048dafc5
Bluette1 <marylene.sawyer@gmail.com>
2023-04-15 08:30:36 +0200

    Style Fixes

--- components/popup.js
+++ components/popup.js
@@ -1,9 +1,24 @@
 import { PopupButton } from "@typeform/embed-react";
+import Image from "next/image";
 
 const Popup = () => {
   return (
-    <PopupButton id="uCWQq4Vg" style={{ fontSize: 20 }} className="my-button">
-      click to open form in popup
+    <PopupButton
+      id="uCWQq4Vg"
+      style={{
+        fontSize: 25,
+        color: "white",
+        fontFamily: "'Lab Grotesque',sans-serif",
+        background: "#102542",
+        border: "solid #102542 1.5px",
+        outlineColor: "#102542",
+        paddingTop: "2.5rem",
+        marginTop: "7.5rem",
+        marginBottom: 0,
+        cursor: "pointer",
+      }}
+    >
+      Click Here To Fill In Healthie Form
     </PopupButton>
   );
 };
